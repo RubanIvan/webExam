2c5d03fa84ecc0787ddb49e7b2522e02aad04808
Ruban Ivan <ivan@ruban.pro>
2015-10-19 15:43:26 +0300

    begin

---
+++ WebExam/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "О сайте";
+            return View();
+        }
+
+        
+    }
+}
